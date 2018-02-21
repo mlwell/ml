c881d31b6dd006798538ed650013f37ef1a590c0
mlwell <hjli100@hotmail.com>
2018-02-20 22:46:19 -0800

    candle stick

--- sml/cattrain.py
+++ sml/cattrain.py
@@ -7,35 +7,40 @@ from keras.optimizers import RMSprop
 from qsdata import qsdata
 import numpy as np
 
-env = qsdata(20)
-x_train, y_train, x_test, y_test = env.get_cat_data(10)
-x_train = np.expand_dims(x_train, axis=2)
-x_test = np.expand_dims(x_test, axis=2)
+#env = qsdata(20)
+#x_train, y_train, x_test, y_test = env.get_cat_data(10)
+#x_train = np.expand_dims(x_train, axis=2)
+#x_test = np.expand_dims(x_test, axis=2)
+
+env = qsdata(1)
+x_train, y_train, x_test, y_test = env.get_candlestick_data(10)
+
 
 model = Sequential()
 
-#model.add(Dense(128, activation='relu', input_dim=20))
-#model.add(Dense(64, activation='relu'))
-
-
-model.add(Conv1D(32, 5, activation='relu', input_shape=(20, 1)))
-model.add(Conv1D(64, 2, activation='relu'))
-#model.add(MaxPooling1D(3))
-#model.add(Conv1D(128, 3, activation='relu'))
-#model.add(Conv1D(128, 3, activation='relu'))
-model.add(GlobalAveragePooling1D())
-#model.add(Dropout(0.5))
-
-model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu', input_dim=4))
 model.add(Dense(64, activation='relu'))
 
-model.add(Dense(1, activation='sigmoid'))
 
-model.compile(loss='binary_crossentropy',
-              optimizer=RMSprop(epsilon=1e-10, rho=0.99, lr=0.002),
+# model.add(Conv1D(32, 5, activation='relu', input_shape=(20, 1)))
+# model.add(Conv1D(64, 2, activation='relu'))
+# #model.add(MaxPooling1D(3))
+# #model.add(Conv1D(128, 3, activation='relu'))
+# #model.add(Conv1D(128, 3, activation='relu'))
+# model.add(GlobalAveragePooling1D())
+# #model.add(Dropout(0.5))
+#
+# model.add(Dense(128, activation='relu'))
+# model.add(Dense(64, activation='relu'))
+
+model.add(Dense(2, activation='sigmoid'))
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=RMSprop(epsilon=1e-10, rho=0.9, lr=0.001),
               metrics=['accuracy'])
 
 model.fit(x_train, y_train, batch_size=64, epochs=10)
 score = model.evaluate(x_test, y_test, batch_size=64)
 
 print ("score", score)
+
